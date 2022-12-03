ff52831803d7dc6616b70039de6216e0f8e28848
Moslem <moslemakhoundi@outlook.com>
2022-12-03 16:36:22 +0330

    conditions-loops-directories

---
+++ 1_conditions/ex_1.py
@@ -0,0 +1,7 @@
+# Write a program to find maximum between two numbers.
+num_1 = int(input("Enter Num 1: "))
+num_2 = int(input("Enter Num 2: "))
+if num_1 > num_2:
+    print(num_1)
+else:
+    print(num_2)
